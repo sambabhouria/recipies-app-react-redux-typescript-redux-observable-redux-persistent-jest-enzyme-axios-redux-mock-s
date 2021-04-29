0f521b096f713eaa9b9db5d86d4a971a38df0360
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2021-04-29 16:39:05 +0200

    cMacBook-Pro:recipes-app

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 //export const API_KEY = "7b4648f6e228423683139533f818a667";
 export const API_KEY = "2240802686264c418a4f6214daacce8d";
 
-export const COUNT_RECIPIES_ON_PAGE = 7;
+export const COUNT_RECIPIES_ON_PAGE = 10;
--- src/views/Footer/Footer.tsx
+++ src/views/Footer/Footer.tsx
@@ -6,7 +6,7 @@ const Footer:React.FC = () => (
   <footer className="d-flex justify-content-center">
   <div className="pb-2 pt-3">
       &copy; {new Date().getFullYear()}, made with{" "}
-      <i className="fa fa-heart heart" /> by <a href="http://github.com/pylnata">pylnata</a>
+      <i className="fa fa-heart heart" /> by <a href="http://github.com/sambabhouria">sambabhouria</a>
    </div>
   </footer>
 );
