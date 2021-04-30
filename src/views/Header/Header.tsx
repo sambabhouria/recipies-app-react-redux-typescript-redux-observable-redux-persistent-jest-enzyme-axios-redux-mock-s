2ab7e6252af49824aa7977788dac9abecf8d7fb7
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2021-04-30 16:21:54 +0200

    MacBook-Pro:recipes-app

--- src/views/Header/Header.tsx
+++ src/views/Header/Header.tsx
@@ -12,16 +12,17 @@ const Header:React.FC = () => {
     <>
       <header className="d-flex justify-content-center align-items-start">
         <img src={Logo} alt="Recipe-App" className="logo" />
-        <div className="title  d-none d-lg-block">sambabhouria's RecipiesApp</div>
+        <div className="title  d-none d-lg-block">Samba recipes App</div>
         <SearchForm />
 
                 <a className="text-success ml-3 mt-2"
                   href="https://github.com/sambabhouria"
                   title="Star on GitHub"
+                  style={{marginRight: '20px'}}
                 >
                   <i className="fab fa-github" />
                 </a>
-                <img src={Logo} alt="Recipe-App" className="logo" />
+                <img src={smababhouria} alt="github-profile" className="logo" />
       </header>
     </>
   );
